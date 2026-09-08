3c8cc405c30ce50a1ce7e3150513eb213c1b3f8d
muntasir17 <muntasirshuvo791@gmail.com>
2026-09-08 20:13:11 +0600

    problem solved

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
-const cart = [
-  { id: 101, name: "Shirt", price: 500 },
-  { id: 102, name: "Pants", price: 800 },
-  { id: 103, name: "Shoes", price: 1200 }
+const products = [
+  { name: "Shirt", category: "Clothing", price: 1000 },
+  { name: "Headphone", category: "Electronics", price: 2000 },
+  { name: "Mouse", category: "Electronics", price: 500 },
+  { name: "Book", category: "Books", price: 300 }
 ];
 
-const removeId = 102;
 let arr = [];
-
-cart.filter((item)=>{
-    if(item.id !== removeId){
-        arr.push(item)
+let sum;
+products.forEach((item)=>{
+    if(item.category === "Electronics"){
+    let discountPrice = (item.price * 10 ) / 100;
+     sum = item.price - discountPrice;
+        item.price = sum;
+        
     }
+    arr.push(item)
 })
 console.log(arr);
+
+
+
+
+
+
 
 
 
