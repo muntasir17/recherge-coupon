31d3c85aa230a39558684b63afae326b128620ca
muntasir17 <muntasirshuvo791@gmail.com>
2026-09-16 17:10:56 +0600

    final

--- script.js
+++ script.js
@@ -2,5 +2,29 @@ let num = document.querySelector(".num");
 let inpFiels = document.querySelector(".inpFiels");
 let btn = document.querySelector(".btn");
 let res = document.querySelector(".res");
+let rando1 = Math.floor(Math.random() * 10000) + 1;
+let rando2 = Math.floor(Math.random() * 10000) + 1;
+let rando3 = Math.floor(Math.random() * 10000) + 1;
+let number = `${rando1}${rando2}${rando3}`;
+num.innerHTML = number
+console.log(number);
+
+
+
+btn.addEventListener("click",()=>{
+let input = inpFiels.value;
+if(input === ""){
+    res.innerHTML = `Enter Number`;
+    setTimeout(() => {
+ res.innerHTML = ` `;
+    }, 1000);
+}else if(input == number){
+    res.innerHTML = `Recherge Successful`;
+}else{
+    res.innerHTML = `Number Mismatch`
+}
+
+})
+
 
 
